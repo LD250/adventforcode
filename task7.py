7f9a19ed1d6985399cded63deb371b4360cb9a7d
Denys Levchenko <denis.levtchenko@gmail.com>
2016-03-04 12:11:03 +0200

    got recursion depth

--- task7.py
+++ task7.py
@@ -6,17 +6,22 @@ def main():
     parsed_code = {}
     init_vars = {}
     for line in lines:
-        left, right = line.split('->')
-        operations = left.strip().split(' ')
-        right = right.strip()
-        if len(operations) == 1:
-            init_vars[right] = int(operations)
-        elif len(operations) == 2:
-            parsed_code[right] = {'vars': [operations[1]],
-                                  'op': [operations[0]]}
-        else:
-            parsed_code[right] = {'vars': [operations[0], operations[2]],
-                                  'op': [operation[1]]}
+        if line:
+            left, right = line.split('->')
+            operations = left.strip().split(' ')
+            right = right.strip()
+            if len(operations) == 1:
+                if operations[0].isdigit():
+                    init_vars[right] = int(operations[0])
+                else:
+                    parsed_code[right] = {'vars': [operations[0]],
+                                        'op': 'INT'}
+            elif len(operations) == 2:
+                parsed_code[right] = {'vars': [operations[1]],
+                                    'op': [operations[0]]}
+            else:
+                parsed_code[right] = {'vars': [operations[0], operations[2]],
+                                    'op': [operations[1]]}
 
     def find_a():
         if 'a' in init_vars:
@@ -25,30 +30,39 @@ def main():
             for var, expression in parsed_code.iteritems():
                 new_vars = []
                 check_var_value = False
-                for var in expression[vars]:
+                for var in expression['vars']:
                     if var in init_vars:
-                        new_var.append(init_vars[var])
+                        new_vars.append(init_vars[var])
                         check_var_value = True
                     else:
-                        new_var.append(var)
-                if check_new_var:
-                    expression[vars] = new_vars
+                        new_vars.append(var)
+                if check_var_value:
+                    expression['vars'] = new_vars
                     op = expression['op']
                     inited_var = None
                     try:
+                        expression['vars'] = [int(v) for v in expression['vars']]
                         if op == 'AND':
-                            pass
+                            inited_var = expression['vars'][0] & expression['vars'][1]
                         elif op == 'OR':
-                            pass
+                            inited_var = expression['vars'][0] | expression['vars'][1]
+                        elif op == 'NOT':
+                            inited_var = ~expression['vars'][0]
+                        elif op == 'INT':
+                            inited_var = int(expression['vars'][0])
+                        elif op == 'LSHIFT':
+                            inited_var = expression['vars'][0] << expression['vars'][1]
+                        elif op == 'RSHIFT':
+                            inited_var = expression['vars'][0] >> expression['vars'][1]
                     except:
                         pass
-                    if inited_var:
+                    if inited_var is not None:
                         init_vars[var] = inited_var
+                        print init_vars
                         del parsed_code[var]
             find_a()
 
-
-
+    find_a()
 
 if __name__ == "__main__":
     main()
