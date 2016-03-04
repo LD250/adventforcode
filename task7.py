8c69d5fd3555f4ac92e274a4344aee225061ef28
Denys Levchenko <denis.levtchenko@gmail.com>
2016-03-04 12:15:50 +0200

    Done

--- task7.py
+++ task7.py
@@ -18,16 +18,16 @@ def main():
                                         'op': 'INT'}
             elif len(operations) == 2:
                 parsed_code[right] = {'vars': [operations[1]],
-                                    'op': [operations[0]]}
+                                    'op': operations[0]}
             else:
                 parsed_code[right] = {'vars': [operations[0], operations[2]],
-                                    'op': [operations[1]]}
+                                    'op': operations[1]}
 
     def find_a():
         if 'a' in init_vars:
-            print(a)
+            print(init_vars['a'])
         else:
-            for var, expression in parsed_code.iteritems():
+            for wire, expression in parsed_code.iteritems():
                 new_vars = []
                 check_var_value = False
                 for var in expression['vars']:
@@ -54,12 +54,10 @@ def main():
                             inited_var = expression['vars'][0] << expression['vars'][1]
                         elif op == 'RSHIFT':
                             inited_var = expression['vars'][0] >> expression['vars'][1]
-                    except:
+                    except Exception as e:
                         pass
                     if inited_var is not None:
-                        init_vars[var] = inited_var
-                        print init_vars
-                        del parsed_code[var]
+                        init_vars[wire] = inited_var
             find_a()
 
     find_a()
